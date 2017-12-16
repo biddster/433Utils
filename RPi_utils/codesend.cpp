c71709d576d6cbb0dc9a31abde62c3fb8e4e0727
biddster <luke.biddell@gmail.com>
2017-12-16 16:23:06 +0000

    Update.

--- RPi_utils/codesend.cpp
+++ RPi_utils/codesend.cpp
@@ -7,13 +7,14 @@
    send(code, length); // where length is always 24 and code is simply the code
    we find using the RF_sniffer.ino Arduino sketch.
 
- Usage: ./codesend decimalcode repeat
+ Usage: ./codesend decimalcode
  (Use RF_Sniffer.ino to check that RF signals are being produced by the RPi's transmitter)
  */
 
 #include "RCSwitch.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) 
 {    
@@ -21,17 +22,20 @@ int main(int argc, char *argv[])
     	// Consult https://projects.drogon.net/raspberry-pi/wiringpi/pins/
    	// for more information.
     	int PIN = 0;
-  
+    
+    	// Parse the firt parameter to this command as an integer
     	int code = atoi(argv[1]);
     	int repeat = atoi(argv[2]);
     
     	if (wiringPiSetup () == -1) return 1;
 	
-	printf("sending code[%i]\n", code);
+	printf("sending code[%i]", code);
 	RCSwitch mySwitch = RCSwitch();
 	mySwitch.enableTransmit(PIN);
     	for(int i = 0; i < repeat; i++) {
     		mySwitch.send(code, 24);
+		usleep(1000);
+		printf(".");
     	}
 	printf("Code[%i] sent [%i]times\n", code, repeat);
 	return 0;
